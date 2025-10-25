518d3e6f62a857e984209184d0016d586ce1dc18
HIEU 6 <hieu.m.nguyen07@gmail.com>
2025-10-25 20:19:28 +0700

    upload Session3 BTVN 03

---
+++ LTC_Session3_BTVN 03.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float r;
+    float chuVi, dienTich;
+
+    printf("Nhap ban kinh hinh tron: ");
+    scanf("%f", &r);
+
+    chuVi = 2 * M_PI * r;
+    dienTich = M_PI * r * r;
+
+    printf("\nChu vi = %.2f\n", chuVi);
+    printf("Dien tich = %.2f\n", dienTich);
+
+    return 0;
+}
